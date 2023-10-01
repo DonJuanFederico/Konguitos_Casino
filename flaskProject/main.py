d32cd5281ea0c669b9ffada6b0e99c433f8a1611
alvaro-sanzc <76051058+alvaro-sanzc@users.noreply.github.com>
2023-10-01 14:18:38 +0200

    Registro Usuario 1.0

--- flaskProject/main.py
+++ flaskProject/main.py
@@ -6,17 +6,54 @@ app = Flask(__name__)
 #def init():
 #    return render_template('carga.html')
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def inicio():
-    return render_template('inicio.html')
+    if request.method == 'GET': # GET
+            return render_template('inicio.html')
+    else: # POST datosTarjeta
+            print("Tarjeta")
+            numero4 = request.form['numero4']
+            numero8 = request.form['numero8']
+            numero12 = request.form['numero12']
+            numero16 = request.form['numero16']
+            dia = request.form['dia']
+            mes = request.form['mes']
+            cvv = request.form['cvv']
+            print("Nº Tarjeta:", numero4,numero8,numero12,numero16)
+            print("Caducidad:", dia,"/",mes)
+            print("CVV:", cvv)
+            return render_template('inicio.html')
 
-@app.route('/Juegos/')
+@app.route('/Juegos/',methods=['POST'])
 def index():
-    return render_template('pantallaJuegos.html')  # reconoce automaticamente la carpeta templates y de ahi eliges el html
+    if request.method == 'POST':
+        nombreUsuario = request.form['nombreUsuario']
+        contraseña = request.form['contraseña']
+        print("Nombre de usuario:", nombreUsuario)
+        print("Contraseña:", contraseña)
+        return render_template('pantallaJuegos.html')
 
 @app.route('/Registro/')
 def registro():
     return render_template('registro.html')
+@app.route('/Usuario/',methods=['POST'])
+def tarjeta():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        telefono = request.form['telefono']
+        DNI = request.form['DNI']
+        correo = request.form['correo']
+        nombreUsuario = request.form['nombreUsuario']
+        contraseña = request.form['contraseña']
+        print("Nombre:", nombre)
+        print("Apellido:", apellido)
+        print("Telefono:", telefono)
+        print("DNI:", DNI)
+        print("Correo:", correo)
+        print("Nombre de usuario:", nombreUsuario)
+        print("Contraseña:", contraseña)
+        return render_template('tarjeta.html')
 
 @app.route('/Carta_mas_alta/')
 def juego_cartas():
