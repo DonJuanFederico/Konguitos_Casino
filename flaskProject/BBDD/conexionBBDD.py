1de5287aa4e80584404ad483a6fc20b5872c6a24
DonJuanFederico <juanfederico.garciaalonsoburon@usp.ceu.es>
2023-10-02 19:34:16 +0200

    metodo agregarDinero

--- flaskProject/BBDD/conexionBBDD.py
+++ flaskProject/BBDD/conexionBBDD.py
@@ -53,3 +53,20 @@ def iniciar_sesion(usuario, contraseña):
             cursor.close()
             close_connection(conn)
     return False  # Devuelve False si no se pudo conectar a la base de datos
+
+def agregarDinero(nombre_usuario, cantidad_a_agregar):
+    conn = connect()
+    if conn:
+        cursor = conn.cursor()
+        try:
+            # Consulta para actualizar la cantidad de dinero en la cuenta del usuario
+            query = "UPDATE usuarios SET Dinero = Dinero + %s WHERE NombreUsuario = %s"
+            cursor.execute(query, (cantidad_a_agregar, nombre_usuario))
+            conn.commit()
+            print(f"Se han agregado {cantidad_a_agregar} unidades de dinero a la cuenta de {nombre_usuario}.")
+        except mysql.connector.Error as err:
+            conn.rollback()
+            print(f"Error de MySQL: {err}")
+        finally:
+            cursor.close()
+            close_connection(conn)
