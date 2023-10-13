0fb2bcd720c742aa7a90edee4f91bfb0f758b18d
alvaro-sanzc <76051058+alvaro-sanzc@users.noreply.github.com>
2023-10-13 19:18:54 +0200

    Camara 1.1 (Foto en BBDD)

--- flaskProject/static/js/camara.js
+++ flaskProject/static/js/camara.js
@@ -1,4 +1,4 @@
-//capturar video ó imagen
+//capturar video o imagen
 const video = document.querySelector(".video");
 const canvas = document.querySelector(".canvas");
 
@@ -8,24 +8,37 @@ const button = document.querySelector(".start-btn");
 //mostrar foto
 const photo = document.querySelector(".photo");
 
+//constrains
 const constraints = {
   video: { width: 420, height: 340 },
   audio: false,
 };
 
+let videoStream; // Variable para mantener un seguimiento del stream de video.
+let isCameraActive = true; // Variable para controlar si la cámara está activa o no.
+
+// Función para acceder a la cámara
 const getVideo = async () => {
   try {
-    const stream = await navigator.mediaDevices.getUserMedia(constraints);
-    handleSucces(stream);
-    console.log(stream);
+    videoStream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(videoStream);
   } catch (error) {
     console.log(error);
   }
 };
 
-const handleSucces = (stream) => {
+// Si la promesa tiene éxito
+const handleSuccess = (stream) => {
   video.srcObject = stream;
   video.play();
+};
+
+// Detener la cámara
+const stopVideoStream = () => {
+  if (videoStream) {
+    const tracks = videoStream.getTracks();
+    tracks.forEach((track) => track.stop());
+  }
 };
 
 getVideo();
