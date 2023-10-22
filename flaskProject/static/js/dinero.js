a037c38aae3b131559424963b68fbb6f7df8d179
Hugo Andrés <hugoandres.herreramiguel@usp.ceu.es>
2023-10-22 12:46:56 +0200

    No retirar más que el salod

--- flaskProject/static/js/dinero.js
+++ flaskProject/static/js/dinero.js
@@ -27,29 +27,32 @@ function agregarDinero() {
 }
 
 function retirarDinero() {
+    let saldoActual = parseFloat(document.getElementById("valorSaldo").textContent);
     var monto = parseFloat(document.getElementById("retirar").value);
     if (!isNaN(monto) && monto > 0) {
         // Muestra un mensaje de confirmación
         if (confirm("¿Estás seguro de que deseas retirar dinero?")) {
-            // Enviar solicitud HTTP a tu servidor Flask
-            var xhr = new XMLHttpRequest();
-            xhr.open("POST", "/retirar_dinero", true);
-            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState === XMLHttpRequest.DONE) {
-                    if (xhr.status === 200) {
-                        alert("Retiro realizado");
-                    } else {
-                        alert("Error al procesar la solicitud");
+            if (monto > saldoActual) {
+                alert("No puedes terirar más de lo que tienes");
+            } else {
+                // Enviar solicitud HTTP a tu servidor Flask
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", "/retirar_dinero", true);
+                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === XMLHttpRequest.DONE) {
+                        if (xhr.status === 200) {
+                            alert("Retiro realizado");
+                        } else {
+                            alert("Error al procesar la solicitud");
+                        }
+                        location.reload(); // Recarga la página
                     }
-                    location.reload(); // Recarga la página
-                }
-            };
-            xhr.send("&cantidad_a_retirar=" + monto);
+                };
+                xhr.send("&cantidad_a_retirar=" + monto);
+            }
         }
-    } else if (monto > saldoActual) {
-        alert("No puedes retirar más dinero del que tienes.");
-    } else {
+    }else {
         alert("Por favor ingresa un número positivo con dos decimales como máximo.");
     }
 }
