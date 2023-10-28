3ff7d3b7363d27fe1972ab245a1fb76501c763ec
Hugo Andrés <hugoandres.herreramiguel@usp.ceu.es>
2023-10-28 18:38:23 +0200

    Interfaz juegos de cartas + cartel cartal mas alta

---
+++ flaskProject/static/js/JuegosCartas.js
@@ -0,0 +1,11 @@
+document.getElementById("cargarBingo").addEventListener("click", function(){
+    window.location.href = "/Juegos/Indice_cartas/Carta_mas_alta/";
+});
+
+document.getElementById("cargarSlots").addEventListener("click", function(){
+    window.location.href = "/Juegos/Indice_cartas/Carta_mas_alta/";
+});
+
+document.getElementById("cargarDino").addEventListener("click", function(){
+    window.location.href = "/Juegos/Indice_cartas/Carta_mas_alta/";
+});
