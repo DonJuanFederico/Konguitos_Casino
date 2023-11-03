2a52bfcd54e7fd0c5e5fae1bd99ee1bb3d6755a8
Hugo Andrés <hugoandres.herreramiguel@usp.ceu.es>
2023-11-03 19:42:02 +0100

    Cartel carta más alta

--- flaskProject/static/js/JuegosCartas.js
+++ flaskProject/static/js/JuegosCartas.js
@@ -2,8 +2,8 @@ document.getElementById("cargarCartaMasAlta").addEventListener("click", function
     window.location.href = "/Juegos/Indice_cartas/Carta_mas_alta/";
 });
 
-document.getElementById("cargarSlots").addEventListener("click", function(){
-    window.location.href = "/Juegos/Indice_cartas/Carta_mas_alta/";
+document.getElementById("cargarBlackJack").addEventListener("click", function(){
+    window.location.href = "/Juegos/Indice_cartas/Blackjack/";
 });
 
 document.getElementById("cargarDino").addEventListener("click", function(){
