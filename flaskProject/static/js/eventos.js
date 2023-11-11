7ef5ba9bc84118d471b03a9cfc33c485b639317e
Tomas Machin <machint.3b@gmail.com>
2023-11-11 12:29:20 +0100

    empezando eventos

---
+++ flaskProject/static/js/eventos.js
@@ -0,0 +1,11 @@
+document.getElementById("cargarCartaMasAlta").addEventListener("click", function(){
+    window.location.href = "/Juegos/Indice_cartas/Carta_mas_alta/";
+});
+
+document.getElementById("cargarBlackJack").addEventListener("click", function(){
+    window.location.href = "/Juegos/Indice_cartas/Blackjack/";
+});
+
+document.getElementById("cargarDino").addEventListener("click", function(){
+    window.location.href = "/Juegos/Indice_cartas/Carta_mas_alta/";
+});
