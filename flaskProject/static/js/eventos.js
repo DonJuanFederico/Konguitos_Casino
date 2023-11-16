2c5fa1f771f13632f7a7bd977179cda0f71b687e
Tomas Machin <machint.3b@gmail.com>
2023-11-16 19:46:21 +0100

    musica perfil y eventos

--- flaskProject/static/js/eventos.js
+++ flaskProject/static/js/eventos.js
@@ -11,6 +11,12 @@ var evento1 = document.getElementById('cargarCartaMasAlta');
 var evento2 = document.getElementById('cargarBlackJack');
 var evento3 = document.getElementById('cargarDino');
 
+// obtenemos todos los valores de todos los temporizadores
+var dias = document.getElementById('dias');
+var horas = document.getElementById('horas');
+var minutos = document.getElementById('minutos');
+var segundos = document.getElementById('segundos');
+
 function unclockEvento(){
     blackout.style.display = 'none';   // quitar cadenas del evento 1
     evento1.classList.add('hover');    // hacemos q el evento del hover este disponible y visible
@@ -28,16 +34,73 @@ function unclockEvento3(){
 }
 function evento_1(){
     if(!bloqueoEvento){
-        window.location.href = '';
+        window.location.href = '';  // GASHAPON
     }
 }
 function evento_2(){
     if(!bloqueoEvento){
-        window.location.href = '';
+        window.location.href = '';  // RULETA RUSA
     }
 }
 function evento_3(){
     if(!bloqueoEvento){
-        window.location.href = '';
+        window.location.href = '';  // EVENTO 3
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Define la fecha y hora de reinicio (lunes a las 00:00:00)
+    const restartTime = new Date();
+    restartTime.setHours(0, 0, 0, 0);
+    restartTime.setDate(restartTime.getDate() + (1 + 7 - restartTime.getDay()) % 7);
+
+    function actualizarTemporizador() {
+        const ahora = new Date();
+        const diaDeLaSemana = ahora.getDay();
+        const horaDelDia = ahora.getHours();
+        // Oculta el temporizador los lunes (día 1) y muestra los demás días
+        const mostrarTemporizador = diaDeLaSemana !== 1; //&& (horaActual < 8 || horaActual >= 17); // el contador se muestra a 0
+
+
+        const tiempoRestante = restartTime - ahora;
+
+        // Calcula días, horas, minutos y segundos restantes
+        const day = Math.floor(tiempoRestante / (1000 * 60 * 60 * 24));
+        const hour = Math.floor((tiempoRestante % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const min = Math.floor((tiempoRestante % (1000 * 60 * 60)) / (1000 * 60));
+        const sec = Math.floor((tiempoRestante % (1000 * 60)) / 1000);
+
+        // Formatea y muestra el temporizador
+        dias.innerHTML = `${day}`;
+        horas.innerHTML = `${hour}`;
+        minutos.innerHTML = `${min}`;
+        segundos.innerHTML = `${sec}`;
+        if(mostrarTemporizador){
+            document.getElementById('temporizador').style.display = 'flex';
+        } else{
+            document.getElementById('temporizador').style.display = 'none';
+        }
+
+        // Si llegamos al reinicio, reinicia el temporizador
+        if (tiempoRestante <= 0) {
+            reiniciarTemporizador();
+        } else {
+            // Sigue actualizando el temporizador en tiempo real
+            requestAnimationFrame(actualizarTemporizador);
+        }
+    }
+    function reiniciarTemporizador() {
+        // Reinicia el temporizador configurando la próxima fecha de reinicio (lunes)
+        restartTime.setDate(restartTime.getDate() + 7);
+        // Actualiza el temporizador inmediatamente después de reiniciar
+        actualizarTemporizador();
+    }
+
+    // Inicializa el temporizador
+    actualizarTemporizador();
+});
+
+/*
+    FALTA Q LOS LUNES QUEDE ABIERTO EL TEMPORIZADOR (DESAPAREZCA)
+    Y CUANDO SEA MARTES VUELVA A APARECER EL TEMPORIZADOR
+ */
