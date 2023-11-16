2b07f7c776d09b06c008e12100563e90a8e088bd
Daniel Escribano <daniel.escribanoissacoivtch@usp.ceu.es>
2023-11-16 13:08:19 +0100

    Soporte al cliente en plantilla

---
+++ flaskProject/static/js/soporte_cliente.js
@@ -0,0 +1,11 @@
+ function submitForm() {
+            var name = document.getElementById('name').value;
+            var email = document.getElementById('email').value;
+            var message = document.getElementById('message').value;
+
+            if(name && email && message) {
+                alert('Gracias, ' + name + '. Hemos recibido tu mensaje y nos pondremos en contacto contigo a la brevedad.');
+            } else {
+                alert('Por favor, completa todos los campos.');
+            }
+        }
