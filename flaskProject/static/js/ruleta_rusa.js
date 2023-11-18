7616ea84d2a0554e457db681d7f1208222dbc822
Daniel Escribano <daniel.escribanoissacoivtch@usp.ceu.es>
2023-11-18 20:34:27 +0100

    Ruleta Rusa version 1.5

--- flaskProject/static/js/ruleta_rusa.js
+++ flaskProject/static/js/ruleta_rusa.js
@@ -1,33 +1,92 @@
-var recamaras = 6;
-var posicionDeBala = Math.floor(Math.random() * recamaras) + 1; //Ponemos +1 para que no salga 0
-var recamaraActual = 1;
+let selectedChamber = null;
+let gameInterval = null;
+function iniciarJuego() {
+    // Oculta el botón de inicio
+    document.getElementById('iniciarJuego').style.display = 'none';
 
-document.getElementById('disparar').disabled = false;
-function disparar() {
-    //si queda solo una recamara restante se acaba el juego
-    if (recamaraActual === recamaras-1) {
-        recamaraActual++;
-        document.getElementById('resultado').innerText = "¡GANASTE!";
-        document.getElementById('balas').innerText = "Recámaras restantes: " + (recamaras - recamaraActual + 1);
-        //bloqueamos el boton de disparar
-        document.getElementById('disparar').disabled = true;
+    //Animacion de encender recamaras:
+    EncenderRecamaras();
+    // Después de 20 segundos, detén el juego automáticamente
+    setTimeout(pararJuego, 10000);
+}
+
+function reiniciarJuegos() {
+    // Reincia el contador
+    let tiempo = 10;
+    document.getElementById('contador').innerHTML = tiempo;
+}
+
+function EncenderRecamaras() {
+    //Iniciamos el contador:
+    contador();
+    let recamaraIluminada = 1;
+    let rondas = 0;
+    gameInterval = setInterval(() => {
+        // Apaga todas las recámaras
+        for (let i = 1; i <= 6; i++) {
+            document.getElementById(`recamara${i}`).style.backgroundColor = "#000000";
         }
-    else if (recamaraActual === posicionDeBala) {
-        document.getElementById('resultado').innerText = "¡¡¡BANG!!!";
-        document.getElementById('balas').innerText = "¡Perdiste!";
-    } else {
-        document.getElementById('resultado').innerText = "¡¡¡Click!!!";
-        recamaraActual++;
-        document.getElementById('balas').innerText = "Recámaras restantes: " + (recamaras - recamaraActual + 1);
-    }
+
+        // Enciende la recámara actual
+        document.getElementById(`recamara${recamaraIluminada}`).style.backgroundColor = "#ff0000";
+
+        // Avanza a la siguiente recámara
+        recamaraIluminada = recamaraIluminada % 6 + 1;
+
+        // Incrementa el contador de rondas cada vez que se completa una ronda
+        if (recamaraIluminada === 1) {
+            rondas++;
+        }
+
+        // Detén el intervalo después de tres rondas
+        if (rondas === 3) {
+            clearInterval(gameInterval);
+            for (let i = 1; i <= 6; i++) {
+                document.getElementById(`recamara${i}`).style.backgroundColor = "#64676e";
+            }
+        }
+    }, 3000 / 18);  // 3 rondas de 6 recámaras en 3 segundos
 }
 
-function retirarse(){
-    //Al darle click al boton retirarse se reinicia el juego
-    recamaras = 6;
-    posicionDeBala = Math.floor(Math.random() * recamaras) + 1; //Ponemos +1 para que no salga 0
-    recamaraActual = 1;
-    document.getElementById('resultado').innerText = "";
-    document.getElementById('balas').innerText = "Recámaras restantes: " + (recamaras - recamaraActual + 1);
-    document.getElementById('disparar').disabled = false;
+function contador() {
+    let tiempo = 10;
+    let intervalo = setInterval(() => {
+        tiempo--;
+        document.getElementById('contador').innerHTML = tiempo;
+        if (tiempo === 0) {
+            clearInterval(intervalo);
+        }
+    }, 999);
 }
+
+function pararJuego() {
+    // Muestra el botón de inicio
+    document.getElementById('iniciarJuego').style.display = 'block';
+    // Apaga todas las recámaras
+    for (let i = 1; i <= 6; i++) {
+        document.getElementById(`recamara${i}`).style.backgroundColor = "#64676e";
+    }
+    // Habilita el botón de inicio
+    document.getElementById('iniciarJuego').disabled = false;
+    //Poner el contador en 10:
+    reiniciarJuegos();
+
+    // Aquí puedes agregar cualquier otra lógica que necesites cuando el juego se detiene
+}
+
+function seleccionarRecamara(numero) {
+    // Si ya se seleccionó una recámara, deselecciónala
+    if (selectedChamber !== null) {
+        document.getElementById(`recamara${selectedChamber}`).style.backgroundColor = '#000000';
+    }
+
+    // Selecciona la nueva recámara
+    selectedChamber = numero;
+    document.getElementById(`recamara${selectedChamber}`).style.backgroundColor = '#c4bd1e';
+}
+
+// Agrega los listeners de click a las recámaras
+for (let i = 1; i <= 6; i++) {
+    document.getElementById(`recamara${i}`).addEventListener('click', () => seleccionarRecamara(i));
+}
+
