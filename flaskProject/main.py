769870f35d4d5e3b7c94ca0f280f3646f33c3e4b
Daniel Escribano <daniel.escribanoissacoivtch@usp.ceu.es>
2023-11-30 19:47:03 +0100

    Desafios y recompensas

--- flaskProject/main.py
+++ flaskProject/main.py
@@ -215,6 +215,7 @@ def perfil():
 def ayuda():
     return render_template('soporte_cliente.html')
 
+@app.route('/desafios_recompensas/')
 def desafios():
     return render_template('desafios_recompensas.html')
 
