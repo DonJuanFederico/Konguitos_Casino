f0e6fc1299d2aa0fdc9510bf363a314f15725698
Hugo Andrés <hugoandres.herreramiguel@usp.ceu.es>
2023-12-01 10:22:10 +0100

    Fondo

---
+++ flaskProject/static/js/juegosDados.js
@@ -0,0 +1,7 @@
+document.getElementById("cargarDino").addEventListener("click", function(){
+    window.location.href = "/Juegos/Juegos_extra/KonguitoRun";
+});
+
+document.getElementById("cargarPlinko").addEventListener("click", function(){
+    window.location.href = "/Juegos/Juegos_extra/Plinko";
+});
