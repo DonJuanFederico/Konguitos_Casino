e1c6d2de954e02de5ef6e6cb93cd5d89e7331181
DonJuanFederico <juanfederico.garciaalonsoburon@usp.ceu.es>
2023-12-01 11:10:46 +0100

    ahora pasan cositas)

--- flaskProject/static/js/plinko.js
+++ flaskProject/static/js/plinko.js
@@ -1,139 +1,87 @@
-/*
-
-document.addEventListener("DOMContentLoaded", function () {
-    const ball = document.getElementById("ball");
-
-    // Set up event listener for clicking on the board
-    document.getElementById("board").addEventListener("click", function () {
-      // Randomly determine the horizontal position of the ball
-      const randomPosition = Math.floor(Math.random() * 100);
-      ball.style.left = `${randomPosition}%`;
-
-      // Reset the ball to the bottom of the board
-      ball.style.bottom = "0";
-
-      // Trigger the animation by setting a timeout
-      setTimeout(function () {
-        // Simulate the ball falling down through the pegs
-        ball.style.bottom = "500px"; // Adjust the height as needed
-      }, 100);
-    });
-  });
-
-
-// PSEUDO CODIGO
-function bolaJugada(){ // codigo q se hace con el atributo onclick de html en la etiqueta (EN BOTON DE JUGAR)
-    // cuando se hace click en jugar (habiendo apostado) se ejecuta esta funcion
-    bola.style.display = 'block';   // muestra la bola en la parte arriba central
-    //function gravedad(parametros_necesarios);   // funcion q aplica la gravedad a la bola
-
-    function path(parametros);  // funcion para determinar el camino q toma la bola
-}
-
-// funcion a aplicar cunado sepamos como van las fisicas
-/*
-function gravedad(lo_q_tenga_q_tener){
-    // hacer q la bola siempre vaya hacia abajo alla donde este
-}
-*/
-/*
-function path(lo_q_tenga_q_tener){
-    // determinar el camino con +1 o -1 si ha tomado el camino de la derecha o izquierda respectivamente
-    for(i = 0; i < 8; i++) {  // q haga la 7 decisiones a tomar (en este caso)
-        setTimeout(() => {  // ponemos un temporizador para tener tiempo de mostrar el cambio en tiempo real de decision
-            math.random ? 1 : -1; // ns si es algo asi pero q randomice la eleccion de +1 o -1
-        }, 1000);
-    }
-    multiplicador_X = true; // hacemos q el multiplicador donde caiga sea true para poder procesar el resultado
-}
-
-function resultado(){
-    // codigo a ejecutar cuando una bola ya ha caido a un multiplicador
-    if(multiplicador_X){
-        // aplicar las recompensas
-    }
-}
-
-/*
-    EN CASO DE NO PONER GRAVEDAD TODAVIA -> hacemos q la bola se desplace hasta el siguiente punto de su path y asi
-                                            respectivamente
-*/
-
-
 document.addEventListener("DOMContentLoaded", function() {
     var ball = document.getElementById("ball");
-    var pegs = document.querySelectorAll(".peg"); // Obtiene todos los elementos de la clase "peg"
+    var pegs = document.querySelectorAll(".peg");
 
     var acceleration = 0.1;
-    var velocity = 0;
+    var velocityX = 0; // Cambiado a velocityX para manejar la velocidad horizontal
+    var velocityY = 0; // Se añadió velocityY para manejar la velocidad vertical
     var audio = new Audio('/static/audio/uff.mp3');
-    // Ajusta la posición inicial de la bola entre un 2% y un 0.1% respecto al centro de la esfera "peg" más cercana al borde superior
+
+   // Ajusta la posición inicial de la bola entre un 2% y un 0.1% respecto al centro de la esfera "peg" más cercana al borde superior
     var initialPositionFactor = Math.random() * (0.02 - 0.001) + 0.001;
     var firstPeg = pegs[0]; // Obtiene el primer elemento del array de esferas "peg"
     ball.style.left = (firstPeg.offsetLeft + firstPeg.clientWidth / 2 - ball.clientWidth / 2) * (1 + initialPositionFactor) + "px";
 
-    // Ajusta el factor de rebote (ajustable según sea necesario)
     var bounceFactor = 0.8;
 
+    var ignoreCollisions = false;
+
     function updateBallPosition() {
-      velocity += acceleration;
-      var currentY = parseFloat(ball.style.top) || 0;
-      var newY = currentY + velocity;
-      ball.style.top = newY + "px";
+        velocityY += acceleration;
+        var currentY = parseFloat(ball.style.top) || 0;
+        var newY = currentY + velocityY;
 
-      // Recorre el array de esferas "peg" para comprobar la colisión con cada una de ellas
-      for (var i = 0; i < pegs.length; i++) {
-        var peg = pegs[i]; // Obtiene el elemento actual del array de esferas "peg"
+        var currentX = parseFloat(ball.style.left) || 0;
+        var newX = currentX + velocityX;
 
-        // Calcular la posición relativa de la bola con respecto al centro de la esfera "peg"
-        var pegCenterX = peg.offsetLeft + peg.clientWidth / 2;
-        var ballCenterX = ball.offsetLeft + ball.clientWidth / 2;
-        var relativePosition = ballCenterX - pegCenterX;
+        ball.style.top = newY + "px";
+        ball.style.left = newX + "px";
 
-        // Verifica la colisión precisa con el objeto de la clase "peg"
-        if (
-          newY + ball.clientHeight >= peg.offsetTop &&
-          newY <= peg.offsetTop + peg.clientHeight &&
-          Math.abs(relativePosition) <= (peg.clientWidth / 2 + ball.clientWidth / 2) && // Añade el radio de la bola al radio de la esfera "peg" al comprobar la distancia entre sus centros
-          ball.offsetLeft + ball.clientWidth >= peg.offsetLeft &&
-          ball.offsetLeft <= peg.offsetLeft + peg.clientWidth
-        ) {
-          // Si hay colisión, ajusta la dirección de la velocidad en función de la posición relativa
-          velocity = -velocity * bounceFactor;
+        for (var i = 0; i < pegs.length; i++) {
+            var peg = pegs[i];
 
-          // Ajusta la posición de la bola para evitar la superposición
-          var distance = Math.sqrt(Math.pow(relativePosition, 2) + Math.pow(velocity, 2)); // Calcula la distancia entre el centro de la bola y el centro de la esfera "peg"
-          var angle = Math.abs(Math.atan2(velocity, relativePosition)); // Usa la función Math.abs para obtener el valor absoluto del ángulo
-          var offset = (peg.clientWidth / 2 + ball.clientWidth / 2) - distance; // Calcula el desplazamiento necesario para evitar la superposición
-          ball.style.top = (ball.offsetTop - Math.sign(velocity) * offset * Math.sin(angle)) + "px"; // Ajusta la posición vertical de la bola según el signo de la velocidad, el desplazamiento y el ángulo
-          ball.style.left = (ball.offsetLeft - Math.sign(relativePosition) * offset * Math.cos(angle)) + "px"; // Ajusta la posición horizontal de la bola según el signo de la posición relativa, el desplazamiento y el ángulo
+            var pegCenterX = peg.offsetLeft + peg.clientWidth / 2;
+            var ballCenterX = ball.offsetLeft + ball.clientWidth / 2;
+            var relativePosition = ballCenterX - pegCenterX;
 
-          // Reproduce un sonido de rebote
-          audio.play();
+            if (
+                !ignoreCollisions &&
+                newY + ball.clientHeight >= peg.offsetTop &&
+                newY <= peg.offsetTop + peg.clientHeight &&
+                Math.abs(relativePosition) <= (peg.clientWidth / 2 + ball.clientWidth / 2) &&
+                newX + ball.clientWidth >= peg.offsetLeft &&
+                newX <= peg.offsetLeft + peg.clientWidth
+            ) {
+                velocityY = -velocityY * bounceFactor;
+                velocityX = velocityX * bounceFactor;
 
-          // Cambia el color de la esfera "peg" a uno de los tres colores indicados
-          var colors = ["#F5F5DC", "#000000", "#D2691E"]; // Array de colores en formato hexadecimal
-          var randomColor = colors[Math.floor(Math.random() * colors.length)]; // Selecciona un color al azar
-          peg.style.backgroundColor = randomColor; // Aplica el color a la esfera "peg"
+                var distance = Math.sqrt(Math.pow(relativePosition, 2) + Math.pow(velocityY, 2));
+                var angle = Math.atan2(velocityY, relativePosition);
+
+                var offset = (peg.clientWidth / 2 + ball.clientWidth / 2) - distance;
+                ball.style.top = (ball.offsetTop - velocityY * Math.sin(angle)) + "px";
+                ball.style.left = (ball.offsetLeft - velocityY * Math.cos(angle)) + "px";
+
+                audio.play();
+
+                var colors = ["#F5F5DC", "#000000", "#D2691E"];
+                var randomColor = colors[Math.floor(Math.random() * colors.length)];
+                peg.style.backgroundColor = randomColor;
+
+                ignoreCollisions = true;
+
+                setTimeout(function() {
+                    ignoreCollisions = false;
+                }, 500);
+            }
         }
-      }
 
-      // Solicita una nueva actualización en el próximo cuadro de animación
-      requestAnimationFrame(updateBallPosition); // Añade esta línea para crear un bucle de animación
+        requestAnimationFrame(updateBallPosition);
     }
 
     document.addEventListener("click", function() {
-      // Cambia la posición inicial de la bola al hacer clic
-      var newPositionFactor = Math.random() * (0.02 - 0.001) + 0.001;
-      ball.style.left = (firstPeg.offsetLeft + firstPeg.clientWidth / 2 - ball.clientWidth / 2) * (1 + newPositionFactor) + "px";
+        var newPositionFactor = Math.random() * (0.02 - 0.001) + 0.001;
+        ball.style.left = (firstPeg.offsetLeft + firstPeg.clientWidth / 2 - ball.clientWidth / 2) * (1 + newPositionFactor) + "px";
 
-      // Resetea la velocidad y la posición de la bola al hacer clic
-      velocity = 0; // Añade esta línea para resetear la velocidad
-      ball.style.top = 0; // Añade esta línea para resetear la posición
+        velocityX = 0;
+        velocityY = 0;
+        ball.style.top = 0;
 
-      requestAnimationFrame(updateBallPosition);
+        requestAnimationFrame(updateBallPosition);
     });
-  });
+
+    updateBallPosition(); // Agregado para iniciar la animación inmediatamente
+});
 
 
 
