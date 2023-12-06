98987f4f3e7f17d06a1e7ccadadb4bada7db4c13
Tomas Machin <machint.3b@gmail.com>
2023-12-06 17:49:12 +0100

    intentando ganancias otra vez

--- flaskProject/static/js/nueva_psswrd.js
+++ flaskProject/static/js/nueva_psswrd.js
@@ -1,6 +1,7 @@
 const numerosCodigo = document.querySelectorAll('.numeros');
 const contenedorVer = document.getElementById('containerVerificacion');
 const contenedorPw = document.getElementById('nuevaPw');
+const requisitos = document.getElementById('requisitos');
 const primeraPw = document.getElementById('primera');
 const segundaPw = document.getElementById('segunda');
 
@@ -28,25 +29,60 @@ numerosCodigo.forEach((input, index) => {
 function verificarContrasena(){
     contenedorVer.style.display = 'none';
     contenedorPw.style.display = 'flex';
+    requisitos.style.display = 'flex';
 }
 
 function comprobarContrasena() {
     if(primeraPw.value === segundaPw.value && primeraPw.value !== '') {
-        var nuevaContrasena = $('#segunda').val();
-        $.ajax({
-            type: 'POST',
-            url: '/Perfil_de_usuario/nueva_pw/',
-            data: {
-                nuevaContrasena: nuevaContrasena
-            },
-            success: function (response) {
-                console.log(response.message);
-                // Puedes realizar acciones adicionales después de cambiar la contraseña
-            },
-            error: function (error) {
-                console.error(error);
-            }
-        });
+        var requisitos = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        if (requisitos.test(segundaPw.value)) {
+            /*var nuevaContrasena = $('#segunda').val();
+            $.ajax({
+                type: 'POST',
+                url: '/Perfil_de_usuario/nueva_pw/',
+                data: {
+                    nuevaContrasena: nuevaContrasena
+                },
+                success: function (response) {
+                    console.log(response.message);
+                    // Puedes realizar acciones adicionales después de cambiar la contraseña
+                },
+                error: function (error) {
+                    console.error(error);
+                }
+            });*/
+            alert('EUREKAAAAA');
+        } else {
+            alert('La contraseña no cumple con los requisitos establecidos.');
+        }
+    } else if(primeraPw.value !== segundaPw.value){
+        alert('Las contraseñas no coinciden.');
+    } else {
+        alert('Introduzca una contraseña valida.')
     }
-    window.location.href = '/Perfil_de_usuario/';
+    //window.location.href = '/Perfil_de_usuario/';
+}
+
+function mostrarRequisitos(){
+    Swal.fire({
+        title: 'Requisitos de contarseña',
+        width: 600,
+        height: 700,
+        html: "<div>" +
+            "<br><strong>1.</strong> Debe tener un mínimo de 8 caracteres." +
+            "<br><strong>2.</strong> Debe contener al menos 1 letra mayúscula." +
+            "<br><strong>3.</strong> Debe contener al menos 1 letra minúscula." +
+            "<br><strong>4.</strong> Debe contener al menos 1 número." +
+            "<br><strong>5.</strong> Debe contener al menos 1 de lo siguientes símbolos: @ $ ! % * ? &." +
+            "</div>",
+        confirmButtonText: 'Entendido',
+        confirmButtonColor: 'goldenrod',
+        backdrop: `rgba(0,0,0,0.4)`,
+        allowOutsideClick: true,
+        allowEscapeKey: true,
+        customClass: {
+            confirmButton: 'custom-button',
+            htmlContainer: 'custom-container'
+          },
+    });
 }
