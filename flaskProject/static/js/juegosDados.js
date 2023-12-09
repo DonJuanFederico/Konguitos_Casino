8dc712a9936367076283205bad51f0a26894278a
Hugo Andrés <hugoandres.herreramiguel@usp.ceu.es>
2023-12-09 21:42:37 +0100

    Arreglo ventana seleccion juegos, add poker, remove dadosPoker

--- flaskProject/static/js/juegosDados.js
+++ flaskProject/static/js/juegosDados.js
@@ -1,7 +1,3 @@
-document.getElementById("cargarPokerDados").addEventListener("click", function(){
-    window.location.href = "/Juegos/Indice_Dados/Poker/";
-});
-
 document.getElementById("cargarCraps").addEventListener("click", function(){
     window.location.href = "/Juegos/Indice_Dados/Craps/";
 });
