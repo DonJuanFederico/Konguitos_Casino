5ad686849f32c03893ec41df53c970e0654b1c4e
Hugo Andrés <hugoandres.herreramiguel@usp.ceu.es>
2023-12-10 12:55:39 +0100

    Nuevo titulo eventos

--- flaskProject/static/js/juegosDados.js
+++ flaskProject/static/js/juegosDados.js
@@ -1,3 +1,7 @@
+document.getElementById("cargarPokerDados").addEventListener("click", function(){
+    window.location.href = "/Juegos/Indice_Dados/Poker/";
+});
+
 document.getElementById("cargarCraps").addEventListener("click", function(){
     window.location.href = "/Juegos/Indice_Dados/Craps/";
 });
