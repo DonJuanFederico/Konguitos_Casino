b0ac1768448966d0d77d1e025f38c05cf569152e
Tomas Machin <machint.3b@gmail.com>
2023-12-11 09:36:25 +0100

    mas arreglos

--- flaskProject/static/js/rankings.js
+++ flaskProject/static/js/rankings.js
@@ -23,54 +23,11 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-
-/*
-document.addEventListener('DOMContentLoaded', function () {
-    // Referencia a la tabla
-    var tabla = document.getElementById('mi-tabla');
-
-    // Realizar solicitud al servidor para obtener datos usando Flask
-    fetch('/obtener_datos')  // Reemplaza '/obtener_datos' con la ruta de tu endpoint Flask
-        .then(response => response.json())
-        .then(datos => {
-            // Generar filas de datos dinámicamente
-            var tbody = tabla.querySelector('tbody');
-            datos.forEach(function (fila) {
-                var tr = document.createElement('tr');
-                tr.innerHTML = `<td>${fila.id}</td><td>${fila.nombre}</td><td>${fila.pais}</td><td>${fila.dinero}</td>`;
-                tbody.appendChild(tr);
-            });
-        })
-        .catch(error => console.error('Error al obtener datos:', error));
-});
-*/
-// ---------------------------------------------------------------------------------------------------------------------
-<!--
-from flask import Flask, render_template, jsonify
-import random
-
-app = Flask(__name__)
-
-# Ruta principal que renderiza la página HTML
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-# Ruta para obtener datos desde el servidor (simulados en este ejemplo)
-@app.route('/obtener_datos')
-def obtener_datos():
-    datos = []
-    for i in range(1, 51):
-        fila = {
-            'id': i,
-            'nombre': f'Persona {i}',
-            'pais': 'País ' + str(random.randint(1, 5)),
-            'dinero': random.randint(100, 1000)
-        }
-        datos.append(fila)
-    return jsonify(datos)
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
--->
+// funcion para ir a la ventana de atras (obtengo la url anterior y voy a ella)
+let prevUrl = document.referrer;
+function volverAtras(){
+    if(prevUrl.indexOf(window.location.host) !== -1) {
+    // Ir a la página anterior
+    window.history.back();
+    }
+}
