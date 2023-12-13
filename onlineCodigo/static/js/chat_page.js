f7775d4902ae4dc30d36530fb466eeb6f225b88b
Hugo Andrés <hugoandres.herreramiguel@usp.ceu.es>
2023-12-13 14:03:12 +0100

    gitignore y cositas

--- onlineCodigo/static/js/chat_page.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let msg = document.querySelector("#user-message");
-    msg.addEventListener("keyup", event => {
-        event.preventDefault();
-        if(event.keyCode === 13){
-            document.querySelector("#send-message").click();
-        }
-    })
-})
